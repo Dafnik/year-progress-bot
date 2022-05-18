1d1d243643ff58613f789495c4f9d36f2997ed39
Dominik <git@dafnik.me>
2022-05-18 08:52:43 +0000

    v2 with uptime monitoring

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-year-progress-bot
+# year progress bot
+[Deployed live version](https://voi.social/@year_progress)
+
+## Setup
+Create a `token.secret` file where you save your mastodon api key
+
+## Run
+Add a cron job which executes `bot.py`
+```bash
+@daily cd /path/to/year_progress/ && python3 bot.py
+```
--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 from mastodon import Mastodon
 from datetime import datetime
+# Only need for uptime monitoring
+#import requests
+
+def close():
+        #print("Sending run uptime ping")
+        #requests.get("https://url", data = {})
+        print("Good bye")
+        quit()
 
 startTime = datetime.now().replace(month=1, day=1)
 endTime = datetime.now().replace(month=12, day=31)
@@ -7,33 +15,34 @@ endTime = datetime.now().replace(month=12, day=31)
 rest = endTime - datetime.now()
 total = endTime - startTime
 percentage = round((1 - rest.total_seconds()/total.total_seconds()) * 100)
+sPercentage = str(percentage)
+
+currentDay = int(datetime.now().strftime("%d"))
+currentMonth = int(datetime.now().strftime("%m"))
+
+sSavedPercentage = open("currentPercentage.txt", "r").readline(2);
+savedPercentage = int(sSavedPercentage)
+
+print("Current day: ", str(currentDay))
+print("Current month: ", str(currentMonth))
+print("Saved percentage: ", sSavedPercentage)
+print("Current percentage: ", sPercentage)
+
 
 if percentage == 100:
-        print("100% triggered");
-        currentDay = int(datetime.now().strftime("%d"))
-        currentMonth = int(datetime.now().strftime("%m"))
-        print("Current day: ", str(currentDay))
-        print("Current month: ", str(currentMonth))
-
+        print("100% case triggered, checking if its the last day of the year");
         if currentDay != 31 or currentMonth != 12:
                 print("It is not the last day of the year! Bye")
-                quit()
+                close()
+                
 
 #   Set up Mastodon
 mastodon = Mastodon(
     access_token = 'token.secret',
-    api_base_url = 'https://voi.social/'
+    api_base_url = "https://mastodon.instance"
 )
 
-f = open("currentPercentage.txt", "r")
-currentPercentage = f.readline(2);
-
-print("Saved percentage: ", currentPercentage)
-print("Current percentage: ", str(percentage))
-
-iCurrentPercentage = int(currentPercentage)
-
-if iCurrentPercentage != percentage:
+if savedPercentage != percentage:
         print("New percentage found!")
 
         percentageToShow = round(percentage / 100 * 20);
@@ -46,20 +55,24 @@ if iCurrentPercentage != percentage:
         for x in range(percentageToShow, 20):
                 toPostMessage = toPostMessage + "░"
 
-        toPostMessage = toPostMessage + " [" + str(percentage) + "%]"
+        toPostMessage = toPostMessage + " [" + sPercentage + "%]"
 
+        print("Message:")
         print(toPostMessage)
 
         mastodon.status_post(toPostMessage)
+        print("Status sent!")
 
-        print("Saving current percentage");
+        print("Saving current percentage...");
         f = open("currentPercentage.txt", "w")
-        f.write(str(percentage))
+        f.write(sPercentage)
         f.close()
-        print("Saving new percentage...");
+        print("Saved");
 
+        #print("Sending post uptime ping")
+        #requests.get("https://monitor.dafnik.me/api/push/MgMUsgO7WO", data = {})
 
 else:
         print("Got nothing to do!");
 
-print ("Good bye")
+close()
